6c6be4eb0a462f13b543af4adb7f041218af999c
Jozef Kudrys <jskudrys@gmail.com>
2020-03-21 01:51:19 +0100

    Added bars
    Added navigation bar and configure page bar

---
+++ public/scripts/keepToggleOpen.js
@@ -0,0 +1,14 @@
+function keepToggleOpen()
+{
+    jQuery('.dropdown-menu.keep-open').on('click', function (e) {
+        e.stopPropagation();       
+    });
+
+    if(1) {
+        $('body').attr('tabindex', '0');
+    }
+    else {
+        alertify.confirm().set({'reverseButtons': true});
+        alertify.prompt().set({'reverseButtons': true});
+    }
+}
